dacb9132ab659d811a8254f8245df246519cd47d
StarDestroyer66 <72447658+SavuStefan@users.noreply.github.com>
2022-09-18 23:41:33 +0300

    Update JSProjects.js

--- JavaScriptProjects/JSProjects.js
+++ JavaScriptProjects/JSProjects.js
@@ -3,8 +3,8 @@ const counter = document.querySelector(".counter");
 let count = 1;
 
 setInterval(()=> {
-    if (count < 5) {
+    if (count < 8) {
         count++;
         counter.innerText = count;
     }
-}, 300);
+}, 200);
